4c69136eecc63dd6b421bd647c1117fbc8a181f2
Jacob Braud <jbraudster@gmail.com>
2019-01-24 05:05:04 -0600

    Trying to commit my project to github without initing it on github

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("are you working?");
+function ask(question, yes, no)
+{
+  if(confirm(question)) yes()
+  else no();
+}
+ask("Do you agree?", () => alert("You agreed."), () => alert("You canceled the execution."));
